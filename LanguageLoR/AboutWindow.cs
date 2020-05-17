d970aa67e53ed18a296b6f93f03ef2bf5712064a
Lucas Carneiro <lucas.carneiro@stoneage.com.br>
2020-05-17 18:37:48 -0300

    Updating links.

--- LanguageLoR/AboutWindow.cs
+++ LanguageLoR/AboutWindow.cs
@@ -9,12 +9,12 @@ namespace LanguageLoR
         private const string VersionText = "Version " + Program.Version;
         private const string AuthorText = "Made by Lucas Rodrigues Carneiro";
         private const string WebsiteText = "Website";
-        private const string WebsiteLink = "https://google.com/search?q=test1";
+        private const string WebsiteLink = "https://github.com/lucas-carneiro/language-lor";
         private const string IssuesText = "Any issues? Report here";
-        private const string IssuesLink = "https://google.com/search?q=test2";
+        private const string IssuesLink = "https://github.com/lucas-carneiro/language-lor/issues";
         private const string IconCreditsText = "Icon made by Freepik from www.flaticon.com";
         private const string LicenseText = "MIT License";
-        private const string LicenseLink = "https://google.com/search?q=test3";
+        private const string LicenseLink = "https://github.com/lucas-carneiro/language-lor/blob/master/LICENSE";
         public AboutWindow()
         {
             InitializeComponent();
